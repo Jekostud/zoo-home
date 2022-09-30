53935221a02c7d325e0eb02955876df3e0a03b6b
Yauheni Chekalkin <70755350+Jekostud@users.noreply.github.com>
2022-09-30 08:20:34 +0300

    add: second week responsive

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,26 @@
+
+  const input = document.querySelector(".range-input");
+  const labels = document.querySelectorAll(".range-label");
+
+  function activeInput(uid) {
+     for (const item of labels) {
+        item.classList.remove("donatLine-active");
+     }
+     labels[uid - 1].classList.add("donatLine-active");
+  }
+
+  if(!!input) {
+     input.addEventListener("change", () => {
+        activeInput(input.value);
+     })
+
+     for (let item in labels) {
+        if (typeof labels[item] === "object") {
+           labels[item].addEventListener("click", () => {
+              const range = +item + 1;
+              input.value = range;
+              activeInput(range);
+           })
+        }
+     }
+  }
