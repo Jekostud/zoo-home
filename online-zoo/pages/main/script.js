b088a162d2a541fb9b4075c8bb93539d9856210a
unknown <tillerman9@gmail.com>
2022-09-30 11:45:13 +0300

    fix:formatted

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,26 +1,25 @@
+const input = document.querySelector(".range-input");
+const labels = document.querySelectorAll(".range-label");
 
-  const input = document.querySelector(".range-input");
-  const labels = document.querySelectorAll(".range-label");
-
-  function activeInput(uid) {
-     for (const item of labels) {
+function activeInput(uid) {
+    for (const item of labels) {
         item.classList.remove("donat_range-active");
-     }
-     labels[uid - 1].classList.add("donat_range-active");
-  }
+    }
+    labels[uid - 1].classList.add("donat_range-active");
+}
 
-  if(!!input) {
-     input.addEventListener("change", () => {
+if (!!input) {
+    input.addEventListener("change", () => {
         activeInput(input.value);
-     })
+    })
 
-     for (let item in labels) {
+    for (let item in labels) {
         if (typeof labels[item] === "object") {
-           labels[item].addEventListener("click", () => {
-              const range = +item + 1;
-              input.value = range;
-              activeInput(range);
-           })
+            labels[item].addEventListener("click", () => {
+                const range = +item + 1;
+                input.value = range;
+                activeInput(range);
+            })
         }
-     }
-  }
+    }
+}
